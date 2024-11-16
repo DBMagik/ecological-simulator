c0e7a8951f0981872a2da44de54891a4be5cac21
DBedell07 <164247151+DBedell07@users.noreply.github.com>
2024-11-16 16:09:40 -0500

    All Models can now be shown in graphs, or raw data

--- Models/disease_spread_model.R
+++ Models/disease_spread_model.R
@@ -2,34 +2,57 @@
 params <- list(
   infection_rate = 0.3,
   recovery_rate = 0.1,
+  death_rate = 0.05,
   initial_susceptible = 990,
   initial_infected = 10,
   initial_recovered = 0
 )
 
+# Time span for the simulation
 time_span <- seq(0, 160, by = 1)
 
+# Load necessary libraries
 library(deSolve)
+library(reshape2)
+library(ggplot2)
 
-
+# Disease Spread Model Function
 disease_spread_model <- function(time, state, parameters) {
   with(as.list(c(state, parameters)), {
     N <- S + I + R
-    dS_dt <- -parameters$beta * S * I / N
-    dI_dt <- parameters$beta * S * I / N - parameters$gamma * I
-    dR_dt <- parameters$gamma * I
+    dS_dt <- -beta * S * I / N
+    dI_dt <- beta * S * I / N - gamma * I - delta * I  # delta represents the death rate
+    dR_dt <- gamma * I
     list(c(dS_dt, dI_dt, dR_dt))
   })
 }
 
 
+# Simulation Function
 simulate_disease_spread <- function(params, time_span) {
-  library(deSolve)
   state <- c(S = params$initial_susceptible,
              I = params$initial_infected,
              R = params$initial_recovered)
   parameters <- list(beta = params$infection_rate,
-                     gamma = params$recovery_rate)
+                     gamma = params$recovery_rate,
+                     delta = params$death_rate)  # New parameter
   out <- ode(y = state, times = time_span, func = disease_spread_model, parms = parameters)
   as.data.frame(out)
 }
+
+
+# Plotting Function
+plot_disease_spread <- function(simulation_results) {
+  # Melt the data for plotting
+  data_melted <- melt(simulation_results, id.vars = "time", variable.name = "Compartment", value.name = "Count")
+  ggplot(data_melted, aes(x = time, y = Count, color = Compartment)) +
+    geom_line(linewidth = 1) +
+    labs(title = "Disease Spread Over Time", x = "Time", y = "Number of Individuals") +
+    theme_minimal()
+}
+
+# Run the simulation
+results <- simulate_disease_spread(params, time_span)
+
+# Plot the results
+plot_disease_spread(results)
